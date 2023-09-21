c20da74a3d6ba4919341453777e63f0ebe75e365
Chrismiweb <chrismibiteso@gmail.com>
2023-09-21 14:16:58 +0100

    landing page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import View from './Components/View/View';
+import Landing from './Components/LandingPage/Landing';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>  
+        <Route path='/' element={<Landing/>}/>
         <Route path='/view' element={<View/>}/>
       </Routes>
     </BrowserRouter>
