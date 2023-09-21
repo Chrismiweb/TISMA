8a550138406673a5019638c5abd900b4b9a112a6
Chrismiweb <chrismibiteso@gmail.com>
2023-09-21 15:15:44 +0100

    latest update

--- src/Components/LandingPage/About.jsx
+++ src/Components/LandingPage/About.jsx
@@ -9,28 +9,28 @@ const About = () => {
         <div className='ting'>
             <div className='grad'>
                 <h1>1.</h1>
-                <div>
+                <div className='grad0'>
                     <h1>Create a business page</h1>
                     <p>You will be able to create a business website page and customize to your business taste.</p>
                 </div>
             </div>
             <div className='grad2'>
                 <h1>2.</h1>
-                <div>
+                <div className='grad0'>
                     <h1>Upload a product</h1>
                     <p>After creating a business page, you will be able to upload your products and manage the products accordingly.</p>
                 </div>
             </div>
             <div className='grad3'>
                 <h1>3.</h1>
-                <div>
+                <div className='grad0'>
                     <h1>Contact form integration</h1>
                     <p>Collect your customers and visitors contact information so as to be able to get in touch with them or they should be able to contact you for a reason.</p>
                 </div>
             </div>
             <div className='grad4'>
                 <h1>4.</h1>
-                <div>
+                <div className='grad0'>
                     <h1>Sell your product online</h1>
                     <p>Having a website or business website page allows you to sell your products online even while you are sleeping!</p>
                 </div>
